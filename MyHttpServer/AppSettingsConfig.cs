9d62788acbe50e7ec3e81ec969c27647e0df1478
foreverestBulat <113981993+foreverestBulat@users.noreply.github.com>
2023-09-22 08:37:33 +0300

    Добавьте файлы проекта.

---
+++ MyHttpServer/AppSettingsConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHttpServer
+{
+    public class AppSettingsConfig
+    {
+        public int Port { get; set; }
+        public string Address { get; set; }
+    }
+}
